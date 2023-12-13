87bd7cc9c3dc20382bae9f7eab8f0382c7e9ad5e
Maddof <97884486+Maddof@users.noreply.github.com>
2023-12-13 10:24:46 +0100

    Added UI to Rock Paper Scissor Game

--- java.js
+++ java.js
@@ -1,84 +1,88 @@
 let playerScore = 0;
 let computerScore = 0;
 
+const parentButtons = document.getElementById("container");
+const resultsSection = document.getElementById("round-results");
+const playerScoreSection = document.getElementById("playerscore");
+const computerScoreSection = document.getElementById("computerscore");
+const computerSelectionSection = document.getElementById("computer-selection");
+
+// Here I use event delegation to prevent propagated outside.
+// This way I can use only one event listener on the parent instead of multiple.
+
+parentButtons.addEventListener("click", (e) => {
+  if (e.target.tagName === "BUTTON") {
+    let playerChoice = e.target.value;
+    console.log("Your choice: " + playerChoice);
+    rpsRound(playerChoice, getComputerChoice());
+  }
+});
+
 function getComputerChoice() {
   const getRandomNumber = Math.floor(Math.random() * 3);
   if (getRandomNumber === 0) {
     let computerChoice = "rock";
-    console.log("Computer Choice: " + computerChoice);
+    computerSelectionSection.innerHTML = `Computer rolled <strong>${computerChoice}</strong>`;
     return computerChoice;
   } else if (getRandomNumber === 1) {
     let computerChoice = "paper";
-    console.log("Computer Choice: " + computerChoice);
+    computerSelectionSection.innerHTML = `Computer rolled <strong>${computerChoice}</strong>`;
     return computerChoice;
   } else {
     let computerChoice = "scissors";
-    console.log("Computer Choice: " + computerChoice);
+    computerSelectionSection.innerHTML = `Computer rolled <strong>${computerChoice}</strong>`;
     return computerChoice;
   }
 }
-
-let getPlayerChoice = () => {
-  let playerChoice = prompt("What do you roll?");
-  playerChoice = playerChoice.toLowerCase();
-  console.log("Your choice: " + playerChoice);
-  return playerChoice;
-};
-
-//getPlayerChoice();
 
 function rpsRound(playerSelection, computerSelection) {
   let result = "";
   if (playerSelection === computerSelection) {
     result = "Tie";
-    console.log(result);
+    resultsSection.innerText = result;
   } else if (playerSelection === "rock" && computerSelection === "scissors") {
     playerScore++;
-    result = "You win!";
-    console.log(result);
+    result = "You win the round!";
+    resultsSection.innerText = result;
+    playerScoreSection.innerText = playerScore;
   } else if (playerSelection === "paper" && computerSelection === "scissors") {
     computerScore++;
-    result = "Computer win!";
-    console.log(result);
+    result = "Computer win the round!";
+    resultsSection.innerText = result;
+    computerScoreSection.innerText = computerScore;
+  } else if (playerSelection === "paper" && computerSelection === "rock") {
+    playerScore++;
+    result = "You win the round!";
+    resultsSection.innerText = result;
+    playerScoreSection.innerText = playerScore;
   } else if (playerSelection === "scissors" && computerSelection === "paper") {
     playerScore++;
-    result = "You win!";
-    console.log(result);
+    result = "You win the round!";
+    resultsSection.innerText = result;
+    playerScoreSection.innerText = playerScore;
+  } else if (playerSelection === "scissors" && computerSelection === "rock") {
+    computerScore++;
+    result = "Computer win the round!";
+    resultsSection.innerText = result;
+    computerScoreSection.innerText = computerScore;
   } else if (playerSelection === "rock" && computerSelection === "paper") {
     computerScore++;
-    result = "Computer win!";
-    console.log(result);
+    result = "Computer win the round!";
+    resultsSection.innerText = result;
+    computerScoreSection.innerText = computerScore;
+  }
+  if (playerScore === 5) {
+    resultsSection.innerText = "PLAYER WINS THE GAME YEAAA!";
+    playerScore = 0;
+    computerScore = 0;
+    playerScoreSection.innerText = "";
+    computerScoreSection.innerText = "";
+  }
+  if (computerScore === 5) {
+    resultsSection.innerText = "COMPUTER WINS THE GAME!";
+    playerScore = 0;
+    computerScore = 0;
+    playerScoreSection.innerText = "";
+    computerScoreSection.innerText = "";
   }
 }
-
-//rpsRound(getPlayerChoice(), getComputerChoice());
-
-function rpsGame() {
-  rpsRound(getPlayerChoice(), getComputerChoice());
-  console.log(
-    `Your current score: ${playerScore}, Compuer current score: ${computerScore}`
-  );
-  console.log("Next round starting...");
-
-  rpsRound(getPlayerChoice(), getComputerChoice());
-  console.log(
-    `Your current score: ${playerScore}, Compuer current score: ${computerScore}`
-  );
-  console.log("Next round starting...");
-
-  rpsRound(getPlayerChoice(), getComputerChoice());
-  console.log(
-    `Your current score: ${playerScore}, Compuer current score: ${computerScore}`
-  );
-  console.log("Next round starting...");
-
-  if (playerScore > computerScore) {
-    console.log("You are the winner of the game. (best of 3)");
-  } else if (playerScore === computerScore) {
-    console.log("Nobody wins best of 3");
-  } else {
-    console.log("Computer is the winner of the game. (best of 3)");
-  }
-}
-
-rpsGame();
